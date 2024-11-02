504d8be7fd04d646e250e358b1f02fd65ec3e548
Sam Coward <idoru42@gmail.com>
2024-11-01 22:57:59 -0400

    consolidate. customize graphics.

--- keyboards/boardsource/lib/oled.c
+++ keyboards/boardsource/lib/oled.c
@@ -2,6 +2,11 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include "oled.h"
 
+#define NUM_FRAMES 4
+#define UPDATE_RATE 2
+
+static int animct = 4;
+
 void render_layer_state(void) {
     switch (get_highest_layer(layer_state)) {
         case 0:
@@ -15,6 +20,11 @@ void render_layer_state(void) {
             break;
         case 3:
             oled_write_raw_P(layer3_img, sizeof(layer3_img));
+            break;
+        case 4:
+            animct ++;
+            int frame_idx = (animct / UPDATE_RATE) % NUM_FRAMES;
+            oled_write_raw_P(layer4_imgs[frame_idx], sizeof(layer4_imgs[frame_idx]));
             break;
     }
 }

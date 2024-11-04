01fe5ea0ef1d365aebcfb71bcd077269b23af317
Sam Coward <idoru42@gmail.com>
2024-11-03 20:39:28 -0500

    animate yin yang on (Shift)-CtrlCmdGUI

--- keyboards/boardsource/lib/oled.c
+++ keyboards/boardsource/lib/oled.c
@@ -6,11 +6,23 @@
 #define UPDATE_RATE 2
 
 static int animct = 4;
+static uint8_t mods;
+
+void animate_frame4(void) {
+    animct ++;
+    int frame_idx = (animct / UPDATE_RATE) % NUM_FRAMES;
+    oled_write_raw_P(layer4_imgs[frame_idx], sizeof(layer4_imgs[frame_idx]));
+}
 
 void render_layer_state(void) {
     switch (get_highest_layer(layer_state)) {
         case 0:
-            oled_write_raw_P(layer0_img, sizeof(layer0_img));
+            mods = get_mods();
+            if ((mods & MOD_MASK_CTRL) && (mods & MOD_MASK_ALT) && (mods & MOD_MASK_GUI)) {
+                animate_frame4();
+            } else {
+                oled_write_raw_P(layer0_img, sizeof(layer0_img));
+            }
             break;
         case 1:
             oled_write_raw_P(layer1_img, sizeof(layer1_img));
@@ -22,9 +34,7 @@ void render_layer_state(void) {
             oled_write_raw_P(layer3_img, sizeof(layer3_img));
             break;
         case 4:
-            animct ++;
-            int frame_idx = (animct / UPDATE_RATE) % NUM_FRAMES;
-            oled_write_raw_P(layer4_imgs[frame_idx], sizeof(layer4_imgs[frame_idx]));
+            animate_frame4();
             break;
     }
 }
